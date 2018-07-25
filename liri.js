f4e43abb26d05a4d5eb7806e20e4e4e6cad28277
patrickfinnigan <patrickfinnigan94@gmail.com>
2018-07-24 18:31:47 -0700

    twitter api has been utilized

--- liri.js
+++ liri.js
@@ -1,8 +1,14 @@
 require("dotenv").config();
 
+const keys = require("./keys")
+// remember to link the keys for your APIs and define the const "keys" for the api consts below!
+
+var Twitter = require("twitter")
 var inquire = require("inquirer")
+
 // const spotify = new Spotify(keys.spotify);
-// const twitter = new Twitter(keys.twitter);
+const twitterClient = new Twitter(keys.twitter);
+// remember to define "keys" with the require() property above!
 
 
 inquire
@@ -13,20 +19,32 @@ inquire
         name: "command"
     }]).then((userInput) => {
         // console.log(userInput.command) 
-        if (userInput.command === "Check my twitter") { // how do i translate the options in the first prompt into a command for userInput?
-            console.log("yeah");
+        if (userInput.command === "Check my twitter") { 
+            // console.log("yeah");
+            const params = {screen_name: "PatrickFinnig10"}
+            twitterClient.get('statuses/user_timeline', params, function(error, tweets) {
+                if (!error) {
+                    for (let i = 0; i < tweets.length; i++) {
+                        console.log(tweets[i].text);
+                        
+                    }
+                }
+            })
         } else if (userInput.command === "Spotify this song"){
-            inquire.prompt ([{
-                    type: "input",
-                    message: "What song do you want to spotify?",
-                    name: "song"
-                }]).then((userInput) => {
-                    if (userInput.song === "") {
-                        console.log("the sign by ace of base");
-                    } 
-                })
-        } else if (userInput.command){
-            console.log("yeah");
+            inquire
+                .prompt ([{
+                        type: "input",
+                        message: "What song do you want to spotify?",
+                        name: "song"
+                    }]).then((userInput) => {
+                        if (userInput.song === "") {
+                            console.log("the sign by ace of base");
+                        } 
+                    })
+        } else if (userInput.command === "Movie this") {
+            console.log("movies");
+        } else if (userInput.command === "Do what it says") {
+            console.log("YOURE HUGE!! THAT MEANS YOU HAVE HUGE GUTS!!")
         }
     })
 
